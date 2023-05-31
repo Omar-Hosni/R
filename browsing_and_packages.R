d6934c03bcd29b50708b152105838ed3f1c3a008
Omar Hosny <68732608+Omar-Hosni@users.noreply.github.com>
2023-05-31 15:13:44 +0200

    Create browsing_and_packages.R

---
+++ browsing_and_packages.R
@@ -0,0 +1,24 @@
+#browsing
+url <- 'http://crantastic.org'
+browseURL(url)
+
+#to let others know you installed a certain package
+install.packages("LiblineaR")
+
+#to see all packages installed
+library()
+
+#to see currently loaded packages
+search()
+
+#to load a package which is installed
+require("LiblineaR")
+
+#to unload a package
+detach("package:LiblineaR", unload=true)
+
+#uninstall the packages
+remove.packages("LiblineaR")
+
+#more info about the package
+? ggplot2
