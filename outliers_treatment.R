d940034cbf546ad07102b43cbc1f57adecc2be3d
Omar Hosny <68732608+Omar-Hosni@users.noreply.github.com>
2023-06-01 05:08:48 +0200

    Update outliers_treatment.R

--- outliers_treatment.R
+++ outliers_treatment.R
@@ -1,3 +1,4 @@
+
 #outliers treatment
 quantile(df$n_hos_rooms, 0.99) #get the 99th percentile
 uv = 3*quantile(df$n_hot_rooms, 0.99)
@@ -6,5 +7,5 @@ df$n_hot_rooms[df$n_hot_rooms > uv] <- uv
 summary(df$n_hot_rooms)
 
 lv = 0.3*quantile(df$rainfall, 0.01)
-df$rainfall(df$rainfall < lv) <- lv
+df$rainfall[df$rainfall < lv] <- lv
 summary(df$rainfall)
